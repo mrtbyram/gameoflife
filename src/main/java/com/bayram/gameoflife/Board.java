d11f8d77f3e842d5265db0b9fd8bba757095efbb
Murat Bayram <murat.bayram@live.com>
2018-11-20 22:49:12 +0300

    BoardCreator

--- src/main/java/com/bayram/gameoflife/Board.java
+++ src/main/java/com/bayram/gameoflife/Board.java
@@ -1,39 +1,61 @@
 package com.bayram.gameoflife;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Board {
 
     private int size;
-    private int[][] cellStatus;
-
-    public Board(int size) {
-        this.size = size;
-        cellStatus = new int[size][size];
-    }
+    private int[][] cellsStatus;
 
     public Board(int size, List<Point> livingPoints) {
         this.size = size;
-        cellStatus = new int[size][size];
+        cellsStatus = new int[size][size];
         initializeCellStatus(livingPoints);
     }
 
     private void initializeCellStatus(List<Point> livingPoints){
-        livingPoints.forEach(point -> cellStatus[point.getX()][point.getY()] = 1);
+        livingPoints.forEach(point -> cellsStatus[point.getX()][point.getY()] = 1);
     }
 
     public int getSize() {
         return size;
     }
 
-    public int displayCell(Point point) {
+    public int displayCellStatus(Point point) {
         if(!isInBoard(point)){
             throw new IndexOutOfBoundsException();
         }
-        return cellStatus[point.getX()][point.getY()];
+        return cellsStatus[point.getX()][point.getY()];
     }
 
     private boolean isInBoard(Point point) {
         return point.getX() <= size && point.getY() <= size;
+    }
+
+    public static BoardCreator withSize(int size) {
+        return new BoardCreator(size);
+    }
+
+    public static class BoardCreator{
+
+        private final int size;
+        private List<Point> livingPoints;
+
+        public BoardCreator(int size) {
+            this.size = size;
+        }
+
+        public Board create() {
+            if(livingPoints == null){
+                livingPoints = Collections.emptyList();
+            }
+            return new Board(size, livingPoints);
+        }
+
+        public BoardCreator withLivingPoints(List<Point> livingPoints) {
+            this.livingPoints = livingPoints;
+            return this;
+        }
     }
 }
--- src/test/java/com/bayram/gameoflife/BoardTest.java
+++ src/test/java/com/bayram/gameoflife/BoardTest.java
@@ -3,7 +3,6 @@ package com.bayram.gameoflife;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -12,17 +11,17 @@ public class BoardTest {
 
     @Test
     public void should_be_initialized_with_size(){
-        Board board = new Board(10);
+        Board board = Board.withSize(10).create();
 
         assertEquals(10, board.getSize());
     }
 
     @Test
     public void should_display_a_cell_status_in_specified_point(){
-        Board board = new Board(2);
+        Board board = Board.withSize(10).create();
 
-        int cell_2_2 = board.displayCell(Point.xy(2, 2));
-        int cell_1_2 = board.displayCell(Point.xy(1, 2));
+        int cell_2_2 = board.displayCellStatus(Point.xy(2, 2));
+        int cell_1_2 = board.displayCellStatus(Point.xy(1, 2));
 
         assertEquals(0, cell_2_2);
         assertEquals(0, cell_1_2);
@@ -30,9 +29,9 @@ public class BoardTest {
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void should_throw_out_of_bound_when_display_out_size(){
-        Board board = new Board(2, Collections.<Point>emptyList());
+        Board board = Board.withSize(2).create();
 
-        board.displayCell(Point.xy(1, 3));
+        board.displayCellStatus(Point.xy(1, 3));
     }
 
     @Test
@@ -40,10 +39,12 @@ public class BoardTest {
         List<Point> livingPoints = new ArrayList<>();
         livingPoints.add(Point.xy(1, 3));
 
-        Board board = new Board(4, livingPoints);
+        Board board = Board.withSize(4)
+                .withLivingPoints(livingPoints)
+                .create();
 
-        assertEquals(1, board.displayCell(Point.xy(1, 3)));
-        assertEquals(0, board.displayCell(Point.xy(2, 2)));
+        assertEquals(1, board.displayCellStatus(Point.xy(1, 3)));
+        assertEquals(0, board.displayCellStatus(Point.xy(2, 2)));
     }
 
 }
