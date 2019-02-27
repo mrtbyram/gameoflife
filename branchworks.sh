333900d34642c21ec3a2d747ec330562bb78e902
mrtbyram <murat.bayram@live.com>
2019-02-27 16:30:08 +0300

    Update branchworks.sh

--- branchworks.sh
+++ branchworks.sh
@@ -3,7 +3,8 @@ then
   git clone --depth=5 https://$gituser:$gitpass@github.com/mrtbyram/gameoflife.git mrtbyram/gameoflife/release
   cd mrtbyram/gameoflife/release
   git checkout master
-  git rebase dev
+  git rebase remotes/origin/dev
+  git remote set-url origin https://$gituser:$gitpass@github.com/mrtbyram/gameoflife.git/
   git push
   echo DEV MERGED TO MASTER
 fi
