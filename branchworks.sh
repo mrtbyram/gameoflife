1db183663f0bbf5f15c33c35ffd1c11a1c6297c2
mrtbyram <murat.bayram@live.com>
2019-02-28 10:57:36 +0300

    Update branchworks.sh

--- branchworks.sh
+++ branchworks.sh
@@ -1,6 +1,7 @@
 echo $TRAVIS_BRANCH
 git clone --depth=5 https://$gituser:$gitpass@github.com/mrtbyram/gameoflife.git mrtbyram/gameoflife/release
 cd mrtbyram/gameoflife/release
+git status
 git merge remotes/origin/dev
 git push
 echo DEV MERGED TO MASTER
