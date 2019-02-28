a9cc8b67f5b2bee2926835b70d69b40484b75ee6
ezbaymu <murat.bayram@ericsson.com>
2019-02-28 10:53:31 +0300

    last changes

--- branchworks.sh
+++ branchworks.sh
@@ -1,10 +1,6 @@
-if [ $TRAVIS_BRANCH == 'dev' ]
-then
-  git clone --depth=5 https://$gituser:$gitpass@github.com/mrtbyram/gameoflife.git mrtbyram/gameoflife/release
-  cd mrtbyram/gameoflife/release
-  git checkout master
-  git merge remotes/origin/dev
-  git remote set-url origin https://$gituser:$gitpass@github.com/mrtbyram/gameoflife.git/
-  git push
-  echo DEV MERGED TO MASTER
-fi
+echo $TRAVIS_BRANCH
+git clone --depth=5 https://$gituser:$gitpass@github.com/mrtbyram/gameoflife.git mrtbyram/gameoflife/release
+cd mrtbyram/gameoflife/release
+git merge remotes/origin/dev
+git push
+echo DEV MERGED TO MASTER
